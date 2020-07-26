87f9d1c2961ebd6bb4e7de7aef0d1a1baef81e2d
Cas <testing@owners-MacBook-Pro-2.local>
2020-07-26 18:31:41 -0500

    Completed assignment w/bonus

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 // Write your JavaScript code here!
 
 //Adding Alerts
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
 
-   fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
-               // Access the JSON in the response
-               response.json().then( function(json) {
-                  //console.log(json[0]); This gets me the first object in the array. Aka Planet Tatooine
-                  const missionTarget = document.getElementById("missionTarget");
-                  function randomPlanet(json) {
-                     return [Math.floor(Math.random() * Math.floor(json.length))];
-                  };
-                  const planetSelected = randomPlanet(json);
-                  missionTarget.innerHTML = `
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
+      // Access the JSON in the response
+      response.json().then(function (json) {
+         //console.log(json[0]); This gets me the first object in the array. Aka Planet Tatooine
+         const missionTarget = document.getElementById("missionTarget");
+
+         function randomPlanet(json) {
+            return [Math.floor(Math.random() * Math.floor(json.length))];
+         };
+         const planetSelected = randomPlanet(json);
+         missionTarget.innerHTML = `
                   <h2> Mission Destination</h2>
                   <ol>
                      <li>Name: ${json[planetSelected].name}</li>
@@ -23,8 +24,8 @@ window.addEventListener("load", function() {
                   </ol>
                   <img src= ${json[planetSelected].image}>`
 
-               });
-            });
+      });
+   });
    /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
@@ -36,23 +37,23 @@ window.addEventListener("load", function() {
 </ol>
 <img src="${}">
 */
-   
-   
+
+
    let form = document.querySelector("form");
-   
-   
-   form.addEventListener("submit", function(event) {
+
+
+   form.addEventListener("submit", function (event) {
       event.preventDefault();
       let pilotName = document.querySelector("input[name=pilotName]");
       let copilotName = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
       let cargoMass = document.querySelector("input[name=cargoMass]");
 
-         if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
-            alert("Oops, you forgot something. All fields are required!");
+      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+         alert("Oops, you forgot something. All fields are required!");
          // stop the form submission
-         
-         }
+
+      }
 
       // let shuttleArrary = []
 
@@ -74,38 +75,38 @@ window.addEventListener("load", function() {
 
 
       //Need to find the data type that is entered into the boxes...
-         // if(typeof(pilotNameValue) !== "string") {
-         //    alert("Uh oh! Someone did not enter valid info :/");
-         //    // stop the form submission
-         //    event.preventDefault();
-         // }
+      // if(typeof(pilotNameValue) !== "string") {
+      //    alert("Uh oh! Someone did not enter valid info :/");
+      //    // stop the form submission
+      //    event.preventDefault();
+      // }
 
-         // if(typeof(copilotNameValue) !== "string") {
-         //    alert("Uh oh! Someone did not enter valid info :/");
-         //    // stop the form submission
-         //    event.preventDefault();
-         // }
-
-         
-         // if (isNaN(fuelLevelValue)) {
-         //    alert("Uh oh! Someone did not enter valid info :/");
-         //    // stop the form submission
-         //    event.preventDefault();
-         // }
+      // if(typeof(copilotNameValue) !== "string") {
+      //    alert("Uh oh! Someone did not enter valid info :/");
+      //    // stop the form submission
+      //    event.preventDefault();
+      // }
 
 
-         // if (isNaN(cargoMassValue)) {
-         //    alert("Uh oh! Someone did not enter valid info :/");
-         //    // stop the form submission
-         //    event.preventDefault();
-         // }
-         
-         // Originally wrote the aobve two conditionals and then realized that I could do the same thing in a single conditional
-         if (isNaN(fuelLevelValue) || isNaN(cargoMassValue)) {
-            alert("Uh oh! Someone did not enter valid info :/");
-            // stop the form submission
-            
-            }
+      // if (isNaN(fuelLevelValue)) {
+      //    alert("Uh oh! Someone did not enter valid info :/");
+      //    // stop the form submission
+      //    event.preventDefault();
+      // }
+
+
+      // if (isNaN(cargoMassValue)) {
+      //    alert("Uh oh! Someone did not enter valid info :/");
+      //    // stop the form submission
+      //    event.preventDefault();
+      // }
+
+      // Originally wrote the aobve two conditionals and then realized that I could do the same thing in a single conditional
+      if (isNaN(fuelLevelValue) || isNaN(cargoMassValue)) {
+         alert("Uh oh! Someone did not enter valid info :/");
+         // stop the form submission
+
+      }
 
       document.getElementById("pilotStatus").innerHTML = `${pilotNameValue} is good to go.`;
       document.getElementById("copilotStatus").innerHTML = `${copilotNameValue} is ready to shake and bake!`;
@@ -114,7 +115,7 @@ window.addEventListener("load", function() {
       let faultyItems = document.getElementById("faultyItems");
       let launchStatus = document.getElementById("launchStatus");
 
-      if (Number(fuelLevelValue)<10000) {
+      if (Number(fuelLevelValue) < 10000) {
          faultyItems.style.visibility = "visible";
          fuelStatus.innerHTML = `${fuelLevelValue}L isn't going to get us far...`;
          launchStatus.innerHTML = `Where Do You Think You're Going???`;
@@ -122,7 +123,7 @@ window.addEventListener("load", function() {
 
       };
 
-      if(Number(cargoMassValue)>10000) {
+      if (Number(cargoMassValue) > 10000) {
          faultyItems.style.visibility = "visible";
          cargoStatus.innerHTML = `Y'all need to lose some weight! ${cargoMassValue} is too much.`;
          launchStatus.innerHTML = `Where Do You Think You're Going???`;
@@ -130,17 +131,17 @@ window.addEventListener("load", function() {
 
       };
 
-      if(Number(fuelLevelValue) >= 10000 && Number(cargoMassValue) <= 10000) {
+      if (Number(fuelLevelValue) >= 10000 && Number(cargoMassValue) <= 10000) {
          faultyItems.style.visibility = "visible";
          launchStatus.innerHTML = `Tonight We Fly!!!`;
          launchStatus.style.color = "green";
       };
 
-      if(cargoMassValue === "") {
+      if (cargoMassValue === "") {
          faultyItems.style.visibility = "hidden";
          launchStatus.innerHTML = "Awaiting Information Before Launch";
          launchStatus.style.color = "orange";
-         launchStatus.style.fontStyle="italic"
+         launchStatus.style.fontStyle = "italic"
       }
 
    });
